4140888fa2f46836a8f25dd397edf4e8878bbd63
Yaozz <jianggongyao@gmail.com>
2020-10-27 11:35:27 +0800

    feat(layers): add conv layer

--- yaonet/parameter.py
+++ yaonet/parameter.py
@@ -5,6 +5,7 @@ from yaonet.tensor import Tensor
 
 
 class Parameter(Tensor):
-    def __init__(self, *shape) -> None:
-        data = np.random.randn(*shape)
+    def __init__(self, shape) -> None:
+        # data = np.random.randn(*shape)
+        data = np.random.standard_normal(shape)
         super().__init__(data, requires_grad=True)
